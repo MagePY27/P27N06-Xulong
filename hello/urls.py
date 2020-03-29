ae9919f2878c1f90ea0225780e7eee2bd2cd889f
P27N06 <xulong@126.com>
2020-03-29 16:40:34 +0800

    init

--- hello/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, re_path
-
-from . import views
-urlpatterns = [
-    path('', views.index, name='index')
-    # re_path('(?P<year>[0-9]{4})/(?P<month>[0-9]{2})/', views.index, name='index')
-]
