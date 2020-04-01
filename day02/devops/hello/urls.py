ec4e8bc5608b554d5237d2af57a2d3efb9d327f3
P27N06 <xulong@126.com>
2020-04-01 19:26:37 +0800

    commit day02-03

--- day02/devops/hello/urls.py
+++ day02/devops/hello/urls.py
@@ -4,5 +4,6 @@ from . import views    #使用主路由+路由时需要导入文件
 urlpatterns = [
     path('', views.index, name='hello'),
     path('list/',views.list, name='list'),
+    path('list2/', views.list2, name='list2'),
 
 ]
--- day02/devops/hello/views.py
+++ day02/devops/hello/views.py
@@ -4,13 +4,26 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     # return HttpResponse("<p>hello Word, Hello, Django!</p>")
-    return render(request, 'index1.html', {'user':'hello Django'})
+    return render(request, 'index.html', {'user':'hello Django'})
 
 def list(request):
     # return HttpResponse('lisfffft')
-    # books = ['python', 'java', 'Django']
-    users = [
-        # {'username':'k1', 'name_cn':'k1', 'age':18},
-        # {'username': 'kf1', 'name_cn': 'kf1', 'age': 128},
+    classname = "Devops"
+    books = ['python', 'java', 'Django']
+    user = {'name':'tom', 'age':18}
+    userlist = [
+        {'username':'zhang3', 'name_cn':'张三', 'age':18},
+        {'username': 'li4', 'name_cn': '李四', 'age': 20},
+        {'username': 'wang5', 'name_cn': '王五', 'age': 25},
     ]
-    return render(request, 'hello/list.html', {'users':users})
+    return render(request, 'list.html', {'classname':classname,'books':books,
+                                         'user':user, 'userlist':userlist})
+
+def list2(request):
+    users = [
+        {'username':'zhang3', 'name_cn':'张三', 'age':18},
+        {'username': 'li4', 'name_cn': '李四', 'age': 20},
+        {'username': 'wang5', 'name_cn': '王五', 'age': 25},
+    ]
+    # return render(request, 'list2.html', {'users':users})
+    return render(request, 'userlist.html', {'users':users})
